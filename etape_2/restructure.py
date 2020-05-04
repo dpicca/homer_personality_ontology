4c7a3c3182eef907dbb73a877d37fb4f2a2e100c
DigitalDW <loris.rimaz@gmail.com>
2020-05-04 13:57:02 +0200

    Change Data Structure

---
+++ etape_2/restructure.py
@@ -0,0 +1,28 @@
+import json
+
+data = None
+with open('../etape_1/hom.od_eng.json', 'r') as f:
+    data = json.load(f)
+
+title = data.get('TEI.2', {}).get('teiHeader', {}).get(
+    'fileDesc', {}).get('titleStmt', {}).get('title')
+
+author = data.get('TEI.2', {}).get('teiHeader', {}).get(
+    'fileDesc', {}).get('titleStmt', {}).get('author')
+
+div = data.get('TEI.2', {}).get('text', {}).get(
+    'body', {}).get('div1')
+
+new_text_data = list()
+for text_data in div:
+    new_text_data.append({
+        "n": text_data.get("@n"),
+        "text": text_data.get("p", {}).get("#text")
+    })
+
+with open('./restructured_data.json', 'w') as f:
+    json.dump({
+        "titre": title,
+        "auteur": author,
+        "text_data": new_text_data
+    }, f)
