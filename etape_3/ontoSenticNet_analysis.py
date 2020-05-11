f5a02597fa50841a6a4608b8057f33574ad22b8c
DigitalDW <loris.rimaz@gmail.com>
2020-05-11 09:59:24 +0200

    revised code structure

--- etape_3/ontoSenticNet_analysis.py
+++ etape_3/ontoSenticNet_analysis.py
@@ -2,12 +2,12 @@ import json
 from nltk.corpus import wordnet as wn
 from SPARQLWrapper import SPARQLWrapper
 
+SPARQL = SPARQLWrapper(
+    endpoint="http://localhost:3030/OntoSenticNet/sparql", returnFormat="json"
+)
+
 
 def main():
-    sparql = SPARQLWrapper(
-        endpoint="http://localhost:3030/OntoSenticNet/sparql", returnFormat="json"
-    )
-
     with open('../etape_2/hom.od_eng_disamb.json', 'r') as f:
         data = json.load(f)
 
@@ -16,15 +16,23 @@ def main():
         for word in text['wsd']:
             if word[2] is not None:
                 word[2] = wn.synset(word[2])
-                searchOntoSenticNetsSentics(word[2], sparql)
+                searchOntoSenticNetsSentics(word[2])
             else:
                 word[2] = None
 
 
-def searchOntoSenticNetsSentics(my_word, sparql):
+def searchOntoSenticNetsSentics(my_word):
     word_data = dict()
     word = my_word.name()
-    ontoSenticNetReadyWord = word[:-5]
+    ontoSenticNetRdyWord = word[:-5]
+
+    word_data["semantics"] = queryOntoSenticNetSemantics(ontoSenticNetRdyWord)
+    word_data["metadata"] = queryOntoSenticNetMetadata(ontoSenticNetRdyWord)
+
+    print(word_data)
+
+
+def queryOntoSenticNetSemantics(word):
     query = """
         PREFIX owl: <http://www.w3.org/2002/07/owl#>
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -35,10 +43,20 @@ def searchOntoSenticNetsSentics(my_word, sparql):
             ?word urn:text "%s".
   	        ?word urn:semantics ?semantics
         }
-    """ % (ontoSenticNetReadyWord)
+    """ % (word)
+    SPARQL.setQuery(query)
+    try:
+        query_result = SPARQL.query().convert()
+        results = query_result.get("results").get("bindings")
+        return [r.get('semantics').get("value").replace(
+            "urn:absolute:ontosenticnet#",
+            ""
+        ) for r in results]
+    except:
+        print('failed')
 
-    word_data["semantics"] = queryOntoSenticNetSemantics(query, sparql)
 
+def queryOntoSenticNetMetadata(word):
     query = """
         PREFIX owl: <http://www.w3.org/2002/07/owl#>
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -52,31 +70,10 @@ def searchOntoSenticNetsSentics(my_word, sparql):
   	        ?word urn:attention ?att.
   	        ?word urn:pleasantness ?plea.
         }
-    """ % (ontoSenticNetReadyWord)
-
-    word_data["metadata"] = queryOntoSenticNetMetadata(query, sparql)
-
-    print(word_data)
-
-
-def queryOntoSenticNetSemantics(query, sparql):
-    sparql.setQuery(query)
+    """ % (word)
+    SPARQL.setQuery(query)
     try:
-        query_result = sparql.query().convert()
-        results = query_result.get("results").get("bindings")
-        return [r.get('semantics').get("value").replace(
-            "urn:absolute:ontosenticnet#",
-            ""
-        ) for r in results]
-        # ret is a stream with the results in XML, see <http://www.w3.org/TR/rdf-sparql-XMLres/>
-    except:
-        print('failed')
-
-
-def queryOntoSenticNetMetadata(query, sparql):
-    sparql.setQuery(query)
-    try:
-        query_result = sparql.query().convert()
+        query_result = SPARQL.query().convert()
         results = query_result.get("results").get("bindings")
         if len(results) > 0:
             r = results[0]
