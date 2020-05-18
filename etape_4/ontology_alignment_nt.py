629320f89154452644f5ddf528a12f83d3d7ec14
DigitalDW <loris.rimaz@gmail.com>
2020-05-18 10:08:10 +0200

    n-triple format test

---
+++ etape_4/ontology_alignment_nt.py
@@ -0,0 +1,84 @@
+import json
+from SPARQLWrapper import SPARQLWrapper
+from nltk.corpus import wordnet as wn
+from rdflib import Graph, Literal, URIRef, Namespace
+from rdflib.namespace import RDF, XSD
+
+
+INPUT_PATH = '../etape_3/hom.od_eng_OntoSenticNet_analysis'
+FILE_FORMAT = '.json'
+with open(INPUT_PATH + FILE_FORMAT, 'r') as input_file:
+    DATA = json.load(input_file)
+
+SPARQL = SPARQLWrapper(
+    endpoint='http://localhost:3030/LemonUby/sparql', returnFormat='json'
+)
+
+GRAPH = Graph()
+
+
+def main():
+    texts_words = DATA.get('analyse_ontoSenticNet')
+    synsets = [{
+        "synset": wn.synset(word["word"]).lemmas()[0].key(),
+        "concept": word["concept"],
+        "sensitivity": word["sensitivity"],
+        "aptitude": word["aptitude"],
+        "attention": word["attention"],
+        "pleasantness": word["pleasantness"],
+    } for words in texts_words.values() for word in words]
+
+    for synset in synsets:
+        synset["synset"] = queryLemonUby(synset["synset"])
+        updateGraph(synset)
+
+    GRAPH.serialize(destination="ontology_alignment", format="nt")
+
+
+def queryLemonUby(synset):
+    synset_type = synset.split("%")[1][0]
+    query = """
+        SELECT *
+        WHERE {
+            ?ref <http://purl.org/olia/ubyCat.owl#externalReference> "[POS: %s] %s".
+        }
+    """ % (getType(synset_type), synset)
+    SPARQL.setQuery(query)
+    try:
+        result = SPARQL.query().convert()
+        reference = result.get("results").get("bindings")[0].get("ref")
+        return reference.get("value").split("#")[0]
+    except:
+        print(query)
+        print("Something went wrong!")
+
+
+def updateGraph(synset):
+    lemon = URIRef(synset["synset"])
+    concept_ref = URIRef("https://sentic.net/api/en/concept/")
+    ontosenticnet = Namespace("urn:absolute:ontosenticnet#")
+
+    GRAPH.add((lemon, concept_ref, Literal(
+        synset["concept"], datatype=RDF.type)))
+    GRAPH.add((lemon, ontosenticnet.pleasantness, Literal(
+        synset["pleasantness"], datatype=XSD.decimal)))
+    GRAPH.add((lemon, ontosenticnet.attention, Literal(
+        synset["attention"], datatype=XSD.decimal)))
+    GRAPH.add((lemon, ontosenticnet.sensitivity, Literal(
+        synset["sensitivity"], datatype=XSD.decimal)))
+    GRAPH.add((lemon, ontosenticnet.aptitude, Literal(
+        synset["aptitude"], datatype=XSD.decimal)))
+
+
+def getType(value):
+    return 'noun' if value == "1" else (
+        'verb' if value == "2" else (
+            'adjective' if value == "3" else (
+                'adverb' if value == "4" else 'adjective'
+            )
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
